0d0a18027f61728fe0bb86d6a0ab4357d3d9d2f9
Mike <mreickcastillero@gmail.com>
2023-10-10 10:26:01 -0700

    added images to assets folder

---
+++ Assets/search.js
@@ -0,0 +1,2 @@
+var streamUrl = 'https://streaming-availability.p.rapidapi.com/search/title?title=Harry Potter&country=us&show_type=all&output_language=en&rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9';
+var imdbLink = 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=TheMummy&rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9&=0';
