13662dcde34f975f86814fe6ecf88ca1d8a93ac9
Mike <mreickcastillero@gmail.com>
2023-10-10 12:15:31 -0700

    added all api data to proper text content and adjusted css for each card displayed

--- Assets/topten.js
+++ Assets/topten.js
@@ -1,11 +1,4 @@
 const url = 'https://imdb188.p.rapidapi.com/api/v1/getWeekTop10?rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9',
-		'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
-	}
-};
 
 function getApi(request){
     fetch(request)
@@ -13,7 +6,14 @@ function getApi(request){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+        console.log("data",data);
+        console.log(data.data[0].plot.plotText.plainText)
+        document.getElementById("imgOne").src = data.data[0].primaryImage.imageUrl
+        document.getElementById("plotOne").textContent = data.data[0].plot.plotText.plainText
+        document.getElementById("oneHeader").textContent = data.data[0].titleText.text;
+        document.getElementById("mediaType").textContent = "type: " + data.data[0].titleType.text;
+        document.getElementById("releaseYearOne").textContent = "Year of Release: "+data.data[0].releaseDate.year;
+        document.getElementById("mediaService").textContent = data.data[0].watchOptionsByCategory.categorizedWatchOptionsList[0].watchOptions[0].title.value;
     })
 }
 
