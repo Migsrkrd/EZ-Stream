e8c93f02484a51a7b8bb03da2b2a54d477a28c9a
nba251522 <inquister112345678@gmail.com>
2023-10-12 10:22:10 -0700

    added a searchtitle function for the search bar

--- Assets/search.js
+++ Assets/search.js
@@ -1,6 +1,29 @@
 var streamUrl = 'https://streaming-availability.p.rapidapi.com/search/title?title=Harry Potter&country=us&show_type=all&output_language=en&rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9';
 var imdbLink = 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=TheMummy&rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9&=0';
 
+
+function getApi(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network error');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.log('There was a problem with the fetching the data:', error.message);
+        });
+}
+
+function searchTitle() {
+    let inputString = document.getElementById('searchInput').value;
+
+    if (!inputString || inputString.trim() === "") {
+        alert("Please enter a valid title");
+        return;
+    }
+}
+
 //add top ten html
 //add top ten css
 //add carousel below header
