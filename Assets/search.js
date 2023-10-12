14add244a35aa0e1bfe3877afdaa799b51f29ab1
nba251522 <inquister112345678@gmail.com>
2023-10-12 10:44:27 -0700

    added a function that will display the results from the api

--- Assets/search.js
+++ Assets/search.js
@@ -22,6 +22,11 @@ function searchTitle() {
         alert("Please enter a valid title");
         return;
     }
+    let formattedUrl = `https://streaming-availability.p.rapidapi.com/search/title?title=${encodeURIComponent(inputString)}&country=us&show_type=all&output_language=en&rapidapi-key=7ddc588895msh7be76c92a64ab8fp1cbaa4jsn721fda1e57e9`;
+    getApi(formattedUrl).then(data => {
+        localStorage.setItem('imdbData', JSON.stringify(data.results));
+        displayResults(data.results);
+    });
 }
 
 //add top ten html
