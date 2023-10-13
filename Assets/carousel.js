5571d488834475cd4c6d87be41bdf2e999e6ecb5
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-12 18:25:12 -0700

    carousel switch to slick library

--- Assets/carousel.js
+++ Assets/carousel.js
@@ -1,2 +1,10 @@
-
-
+$(document).ready(function() {
+    $('.orbit-container').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        dots: true
+    });
+});     
