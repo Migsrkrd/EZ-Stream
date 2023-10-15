af03d7201823eee32874704a6cd0c32ee1b30737
nba251522 <inquister112345678@gmail.com>
2023-10-15 13:54:11 -0700

    changed the webpage so that now if the movie detail html doesnt have any streaming services utilized, it will display a message saying that the movie/show isnt available. I also made it so that the poster on that page is replaced with a placehold if tmdb didnt have one.

--- Assets/details.js
+++ Assets/details.js
@@ -14,7 +14,10 @@ document.addEventListener('DOMContentLoaded', function() {
         try {
             const response = await fetch(`https://api.themoviedb.org/3/${type}/${id}?api_key=6a68f26813b3c5095ce117f409c6693c&language=en-US`);
             const data = await response.json();
-            document.getElementById('moviePoster').src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+
+            const posterImagePath = data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : 'Assets/posterimage.png';
+    
+            document.getElementById('moviePoster').src = posterImagePath;
             document.getElementById('movieTitle').textContent = data.title || data.name; 
             document.getElementById('movieReleaseDate').textContent = data.release_date || data.first_air_date; 
             const movieRatingElement = document.getElementById('movieRating');
@@ -33,14 +36,26 @@ document.addEventListener('DOMContentLoaded', function() {
             const response = await fetch(`https://api.themoviedb.org/3/${type}/${id}/watch/providers?api_key=6a68f26813b3c5095ce117f409c6693c`);
             const data = await response.json();
             const providers = data.results.US ? data.results.US.flatrate : [];
-            const topProviders = providers.slice(0, 3);        
-            const providerList = document.createElement('ul');
-            topProviders.forEach(provider => {
-                const listItem = document.createElement('li');
-                listItem.textContent = provider.provider_name;
-                providerList.appendChild(listItem);
-            });
-            document.querySelector('.movie-details').appendChild(providerList);
+            
+            const streamingServicesList = document.getElementById('streamingServicesList');
+    
+            // First, clear the existing content if any
+            streamingServicesList.innerHTML = '';
+    
+            if (providers.length === 0) {
+                // If there are no providers available, display a placeholder message
+                const placeholderMessage = document.createElement('li');
+                placeholderMessage.textContent = 'This movie/TV show is not available on any streaming services at the moment.';
+                streamingServicesList.appendChild(placeholderMessage);
+            } else {
+                // Otherwise, display the available streaming providers
+                const topProviders = providers.slice(0, 3);        
+                topProviders.forEach(provider => {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = provider.provider_name;
+                    streamingServicesList.appendChild(listItem);
+                });
+            }
         } catch (error) {
             console.error("Error fetching providers:", error);
         }
